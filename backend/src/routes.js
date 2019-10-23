d7358ef94e220205848d0b65e53a4ad22be6ec6a
Fabiano Vilela <fabiano.vilela@gmail.com>
2019-10-23 13:34:16 -0300

    Authorization

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/Auth';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
